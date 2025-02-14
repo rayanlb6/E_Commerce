c9c6a12615ff9c2fa685a07609757c9f4372e9a0
rayanlb6 <toukaprayan6@gmail.com>
2025-02-14 15:42:26 +0100

    Add function 'search' for research and another function for category details

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product,Category
 
 class LatestProductsList(APIView):
     def get(self,request, format=None):
@@ -24,5 +26,28 @@ class ProductDetail(APIView):
         product= self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-
+class CategoryDetail(APIView):
+    def get_object(self,category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404
     
+    def get(self, request, category_slug, format=None):
+        category= self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+@api_view(['POST'])
+
+def search(request):
+    query = request.data.get('query','')
+
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains = query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
+    
+ 
